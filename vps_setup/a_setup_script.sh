76304b00950e4a3d3bbc19d8393a273554b38ffa
gangh <gsteel@foxmail.com>
2017-04-08 00:13:44 +0800

    deploy shadowsockd

--- vps_setup/a_setup_script.sh
+++ vps_setup/a_setup_script.sh
@@ -11,6 +11,9 @@ apt -y install tmux tree htop git hub wget bzip2
 wget https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
 bash Anaconda3-4.3.1-Linux-x86_64.sh 
 
+# install shadowsocks server
+pip install https://github.com/gangh/shadowsocks/archive/master.zip
+
 # NOTE: This is an example that sets up SSH authorization. To use it, you'd need to replace "ssh-rsa AA... youremail@example.com" with your SSH public.
 # You can replace this entire script with anything you'd like, there is no need to keep it
 
